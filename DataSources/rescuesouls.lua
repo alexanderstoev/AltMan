7fcccc6419f85fabb30509ac82ff12a43883a3a2
Alex Stoev <alex.stoev@gmail.com>
2020-12-04 23:00:40 +0200

    Added rescue souls quests  data source

---
+++ DataSources/rescuesouls.lua
@@ -0,0 +1,38 @@
+local _, AltMan = ...;
+
+if (AltMan.DataSources== nil) then
+    AltMan.DataSources = {}
+end
+
+AltMan.DataSources.rescuesouls = function () 
+
+    -- anima reservoir quest IDs
+    -- https://shadowlands.wowhead.com/search?q=Replenish+the+Reservoir
+    local questIDs = {
+        61331,
+        61332,
+        61333,
+        61334,
+    }
+    
+    for _, questID in pairs(questIDs) do 
+
+        -- check if the quest is marked as completed
+        if (C_QuestLog.IsQuestFlaggedCompleted(questID)) then
+            return AltMan.translations["en"]["done"];
+        
+        -- check if the quest is marked as completed
+        elseif (C_QuestLog.IsQuestFlaggedCompleted(questID)) then
+            return AltMan.translations["en"]["done"];
+
+        -- check if we have the quest in the log
+        elseif (not(C_QuestLog.GetLogIndexForQuestID(questID) == nil)) then
+            local data = C_QuestLog.GetQuestObjectives(questID)[1]
+            if (not(data == nil)) then
+                return data.numFulfilled .. "/" .. data.numRequired;
+            end 
+        end
+    end
+
+    return AltMan.translations["en"]["notfound"];
+end
