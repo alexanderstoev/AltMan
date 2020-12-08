859ade1f64a553af7849e59eba59371f188b05d7
Alex Stoev <alex.stoev@gmail.com>
2020-12-08 16:46:11 +0200

    Added Daily and Weekly resets data sources

--- AltManTranslations.lua
+++ AltManTranslations.lua
@@ -21,5 +21,20 @@ AltMan.translations = {
         ["notdone"] = "To do",
         ["notfound"] = "Not found",
         ["secondquestnotfound"] = "Second quest not found",
+
+        ["maininfo"] = {
+            ["weeklyreset"] = {
+                ["weeklyreset"] = "Weekly reset: ", -- SHOULD INCLUDE WHITE SPACE
+                ["today"] = "TODAY",
+                ["in"] = "In ", -- as in "In 2 days" SHOULD INCLUDE WHITE SPACE
+                ["days"] = " days", -- as in "In 2 days" SHOULD INCLUDE WHITE SPACE
+            },
+            ["dailyreset"] = {
+                ["daily"] = "Daily reset: ", -- SHOULD INCLUDE WHITE SPACE
+                ["lessthanhour"] = "Less than an hour",
+                ["in"] = "In ", -- as in "In 2 hours" SHOULD INCLUDE WHITE SPACE
+                ["hours"] = " hours", -- as in "In 2 hours" SHOULD INCLUDE WHITE SPACE
+            }
+        }
     }
 }
