644132d00a36ea6574713a13caef90a886a7c8a4
Alex Stoev <alex.stoev@gmail.com>
2020-12-10 07:50:23 +0200

    Added sorted pairs method

--- AltManHelpers.lua
+++ AltManHelpers.lua
@@ -19,3 +19,43 @@ end
 function trim(s)
 	return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
+
+
+----------------------------------------------------------------------------
+-- iterates over the table in a sorted order
+-- more info on stackoverflow:
+-- https://stackoverflow.com/questions/15706270/sort-a-table-in-lua
+----------------------------------------------------------------------------
+function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
+
+function compareAlts(t,a,b)
+	if (a == AltMan.currentAltGUID) then
+		return true;
+	end
+	if (t[a].name < t[b].name) then
+		return true
+	end
+	return false
+end
