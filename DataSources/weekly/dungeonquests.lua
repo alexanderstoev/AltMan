ea7147f02948c845b6909a08b9c14d53e0dc6881
Alex Stoev <alex.stoev@gmail.com>
2020-12-10 07:54:19 +0200

    Added check if the the quest exists

--- DataSources/weekly/dungeonquests.lua
+++ DataSources/weekly/dungeonquests.lua
@@ -4,7 +4,7 @@ if (AltMan.DataSources== nil) then
     AltMan.DataSources = {}
 end
 
-AltMan.DataSources.dungeonquests = function () 
+AltMan.DataSources.dungeonquests = function ()
 
     -- dungeon quest IDs
     -- https://shadowlands.wowhead.com/search?q=Trading+Favors
@@ -23,33 +23,35 @@ AltMan.DataSources.dungeonquests = function ()
 
     local returnSrings = {}
 
-    for _, questID in pairs(questIDs) do 
-        
+    for _, questID in pairs(questIDs) do
+
         local questTitle = C_QuestLog.GetTitleForQuestID(questID);
+
         if (not(questTitle == nil)) then
+
             questTitle = string.gsub(questTitle, removeString, "")
-            
+
             -- check if the quest is marked as completed
             if (C_QuestLog.IsComplete(questID)) then
                 table.insert(returnSrings, questTitle .. ": " .. AltMan.translations["en"]["done"]);
-                
+
             -- check if the quest is marked as completed
             elseif (C_QuestLog.IsQuestFlaggedCompleted(questID)) then
                 table.insert(returnSrings, questTitle .. ": " .. AltMan.translations["en"]["done"]);
-                
+
             -- check if we have the quest in the log
             elseif (not(C_QuestLog.GetLogIndexForQuestID(questID) == nil)) then
                 table.insert(returnSrings, questTitle .. ": " .. AltMan.translations["en"]["notdone"]);
             end
         end
-        
+
     end
 
     local foundQuests = sizeOfTable(returnSrings);
-    if (foundQuests == 0) then 
+    if (foundQuests == 0) then
         return AltMan.translations["en"]["notfound"];
     end
-    if (foundQuests == 1) then 
+    if (foundQuests == 1) then
         table.insert(returnSrings, AltMan.translations["en"]["secondquestnotfound"])
     end
 
