3543f364d332c9680b2b65b3eacdf1d72a7e4d0e
Alex Stoev <alex.stoev@gmail.com>
2020-12-14 10:59:54 +0200

    minor look and feel change

--- UI/AltManConst.lua
+++ UI/AltManConst.lua
@@ -7,10 +7,13 @@ AltMan.constants.presentation.fontSize = 13
 AltMan.constants.presentation.textPadding = 10
 AltMan.constants.presentation.lineheight = AltMan.constants.presentation.fontSize + AltMan.constants.presentation.textPadding
 
+AltMan.constants.presentation.mainFrameGrey = 0.15
+AltMan.constants.presentation.headerGrey = 0.1
+AltMan.constants.presentation.evenCharGrey = 0.1
 
 AltMan.constants.presentation.labelsFrameWidth = 180
 AltMan.constants.presentation.altFrameWidth = 200
-AltMan.constants.presentation.frameAlpha = 0.7
+AltMan.constants.presentation.frameAlpha = 0.8
 
 AltMan.constants.presentation.header = {}
 AltMan.constants.presentation.header.height = 30
