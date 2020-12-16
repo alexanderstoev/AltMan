efe19bdf3d0ccbef0373ad9e7021ca2da12bcb2d
Alex Stoev <alex.stoev@gmail.com>
2020-12-16 10:47:58 +0200

    Fix for world bosses quests

--- DataSources/weekly/worldboss.lua
+++ DataSources/weekly/worldboss.lua
@@ -14,13 +14,45 @@ AltMan.DataSources.worldboss = function()
     for _, questID in pairs(questIDs) do
 
         local questTitle = C_TaskQuest.GetQuestInfoByQuestID(questID);
+        local remainingMinutes = C_TaskQuest.GetQuestTimeLeftMinutes(questID);
 
-        if (C_QuestLog.IsComplete(questID)) then
-            table.insert(returnSrings, questTitle .. ":\n" .. AltMan.translations["en"]["done"] .. "|cFF00FF00");
+        -- check if the quest is active
+        if (not (remainingMinutes == nil)) then
+
+            -- chck if the quest is taken
+            if (not C_QuestLog.IsOnQuest(questID)) then
+                table.insert(returnSrings,
+                    questTitle .. ":\n" .. AltMan.translations["en"]["nottaken"] .. ' - ' ..
+                        GetRemainingTime(remainingMinutes * 60) .. " remaining");
 
             -- check if the quest is marked as completed
-        elseif (C_QuestLog.IsQuestFlaggedCompleted(questID)) then
-            table.insert(returnSrings, questTitle .. ":\n" .. AltMan.translations["en"]["done"] .. "|cFF00FF00");
+            elseif (C_QuestLog.IsComplete(questID)) then
+                table.insert(returnSrings, questTitle .. ":\n" .. AltMan.translations["en"]["done"]);
+
+            -- check if the quest is marked as completed
+            elseif (C_QuestLog.IsQuestFlaggedCompleted(questID)) then
+                table.insert(returnSrings, questTitle .. ":\n" .. AltMan.translations["en"]["done"]);
+
+            -- check if we have the quest in the log
+            elseif (not (C_QuestLog.GetLogIndexForQuestID(questID) == nil)) then
+                local data = C_QuestLog.GetQuestObjectives(questID)[1]
+
+                if (not (data == nil)) then
+                    if (data.type == "progressbar") then
+                        table.insert(returnSrings,
+                            questTitle .. ":\n" .. GetQuestProgressBarPercent(questID) .. "%" .. ' - ' ..
+                                GetRemainingTime(remainingMinutes * 60) .. " remaining");
+                    else
+                        table.insert(returnSrings,
+                            questTitle .. ":\n" .. data.numFulfilled .. "/" .. data.numRequired .. ' - ' ..
+                                GetRemainingTime(remainingMinutes * 60) .. " remaining");
+                    end
+                end
+            else
+                table.insert(returnSrings,
+                    questTitle .. ":\n" .. GetRemainingTime(remainingMinutes * 60) .. " remaining");
+            end
+
         end
     end
 
